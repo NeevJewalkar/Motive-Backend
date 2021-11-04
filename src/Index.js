e12be7d2780b6d7a83c50f8058c385eb7c08317d
NeevJewalkar <neevkar@icloud.com>
2021-11-04 13:56:18 +0530

    Added basic User Auth

--- src/Index.js
+++ src/Index.js
@@ -43,6 +43,51 @@ app.post('/get', (req, res) => {
     })
 })
 
+/* User API functions */
+
+app.post('/user/create', (req, res) => {
+    client.connect(process.env.DB_URL, (err, db) => {
+        if (err) throw err;
+
+        let dbo = db.db('Motive')
+        let collection = dbo.collection('users')
+
+        let newUser = {
+            Username: req.get('Username'),
+            Password: req.get('Password'),
+            Email: req.get('Email'),
+        }
+
+        collection.insertOne(newUser, (err, result) => {
+            if (err) {
+                console.log(err)
+                res.send(err)
+            }
+            res.send(result)
+        })
+    })
+})
+
+app.post('/user/get/', (req, res) => {
+    client.connect(process.env.DB_URL, (err, db) => {
+        if (err) throw err;
+
+        let dbo = db.db('Motive')
+        let collection = dbo.collection('users')
+
+        collection.find({ Username: req.get('Username'), Password: req.get('Password') }).toArray((err, docs) => {
+            if (err) throw err;
+
+            if (docs[0] === undefined) {
+                res.json({ Message: 'User or Password is Invalid' })
+                break;
+            } else {
+                res.json({ Email: docs[0].Email })
+            }
+        })
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
